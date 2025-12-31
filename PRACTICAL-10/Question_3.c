b7e65feb6d7cbbeadc9fb6abfeae4a942f7a4ff7
aryanraj-creator <aryan.raj@rungta.org>
2025-12-31 17:36:09 +0530

    added new file

---
+++ PRACTICAL-10/Question_3.c
@@ -0,0 +1,29 @@
+//Row-wise Sum of 2D Array
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j;
+    int a[10][10];
+    int sum;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++) {
+        sum = 0;
+        for(j = 0; j < c; j++) {
+            sum += a[i][j];
+        }
+        printf("Sum of row %d = %d\n", i + 1, sum);
+    }
+
+    return 0;
+}
+        
---
+++ PRACTICAL-9/Question_2.c
@@ -0,0 +1,30 @@
+//Maximum and Minimum Element of an Array
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a[100];
+    int max, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    max = min = a[0];
+
+    for(i = 1; i < n; i++) {
+        if(a[i] > max)
+            max = a[i];
+        if(a[i] < min)
+            min = a[i];
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
